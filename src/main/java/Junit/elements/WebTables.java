16e10ccc665bfd9ee11ac3d25ff3599a66721e28
HiMan29 <himanshu29uk@gmail.com>
2023-08-10 10:44:12 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/java/Junit/elements/WebTables.java
+++ src/main/java/Junit/elements/WebTables.java
@@ -5,11 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebTables {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         WebDriver driver = new ChromeDriver();
         driver.get("https://demoqa.com/webtables");
         driver.manage().window().maximize();
 
+        System.out.println("Deatils added");
         driver.findElement(By.id("addNewRecordButton")).click();
         driver.findElement(By.id("firstName")).sendKeys("him");
         driver.findElement(By.id("lastName")).sendKeys("hohksaf");
@@ -17,7 +18,16 @@ public class WebTables {
         driver.findElement(By.id("age")).sendKeys("89");
         driver.findElement(By.id("salary")).sendKeys("13344");
         driver.findElement(By.id("department")).sendKeys("hsfghjhgh");
-        driver.findElement(By.id("submit'")).click();
+        driver.findElement(By.id("submit")).click();
+
+        System.out.println("edit Details");
+
+        driver.findElement(By.id("searchBox")).sendKeys("Vega");
+        driver.findElement(By.xpath("//*[name()='path' and contains(@d,'M880 836H1')]")).click();
+        driver.findElement(By.id("firstName")).clear();
+        driver.findElement(By.id("firstName")).sendKeys("HImmmaa");
+        driver.findElement(By.id("submit")).click();
+        driver.close();
 
     }
 }
